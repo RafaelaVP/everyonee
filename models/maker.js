ad2589ed8ac75f8f67dc7bde4d0a541567bf14a4
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-02-25 17:40:26 -0300

    refact: adjust maker

--- models/maker.js
+++ models/maker.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const company = require('./company');
 
 module.exports = function(sequelize, DataTypes) {
   const Maker = sequelize.define('maker', {
@@ -15,15 +16,19 @@ module.exports = function(sequelize, DataTypes) {
   employeeId:{
     type: Sequelize.INTEGER,
       allowNull:false
+  },
+  companyId: {
+    type: Sequelize.INTEGER,
+      allowNull:false
   }
   })
   Maker.associate = function(models) {
     Maker.belongsTo(models.employee, {foreignKey: 'employeeId', as: 'employees'})
-  };
-  
-  Maker.associate = function(models) {
+    Maker.belongsTo(models.company, {foreignKey: 'companyId', as: 'companies'})
     Maker.hasMany(models.product, {as: 'products'})
-  }
+
+  };
+
 
   return Maker;
 }
