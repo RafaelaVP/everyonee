0d2f16fa14b4fd71193ef9f0a073c2ad1a9d5257
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-02-25 17:37:12 -0300

    refact: add primary key, product, employee, maker

---
+++ migrations/20220225183416-create-add-references-product-company.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('products', 'companyId', {
+      type: Sequelize.INTEGER,
+        references: {
+          model: 'companies',
+          key: 'id'
+        }
+     })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('products', 'companyId', {
+      type: Sequelize.INTEGER,
+     })
+  }
+};
